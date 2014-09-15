2ac7d4f592af9d88f5bc4409e3a108963f8b4abd
wildaan46 <dan_wil@rocketmail.com>
2014-09-15 16:00:22 +0700

    commit

---
+++ kurang.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class kurang {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		int hasil;
+		System.out.println("bilangan 1:");
+		a = sc.nextInt();
+		System.out.println("bilangan 2:");
+		b = sc.nextInt();
+		hasil = a - b;
+		System.out.println("Hasil :" +hasil);
+
+	}
+}
